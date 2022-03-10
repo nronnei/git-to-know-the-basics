3f12e84e1879e898a3d87da394ad4326e511c5cc
Nick Ronnei <nronnei@gmail.com>
2022-03-10 08:40:29 -0500

    Moves command code to lib

--- index.mjs
+++ index.mjs
@@ -3,18 +3,17 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
+import hello from './lib/hello.mjs';
+
+// invocation example: http://yargs.js.org/
 yargs(hideBin(process.argv))
   .scriptName("star-wars-parser")
   .usage('$0 <cmd> [args]')
-  // invocation example: http://yargs.js.org/
-  .command('hello [name]', 'welcome [name] to Tatooine', (yargs) => {
-    yargs.positional('name', {
-      type: 'string',
-      default: 'Luke Skywalker',
-      describe: 'the name to say hello to'
-    })
-  }, function (argv) {
-    console.log(`Hello there, ${argv.name}! *he said, all Obi-wan-like*`)
-  })
+  .command(
+    hello.cmd,
+    hello.desc,
+    hello.builder,
+    hello.handler
+  )
   .help()
   .argv
